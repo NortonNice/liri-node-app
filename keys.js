4de9b71eb6fc044cd7a1fa32fbbb26394bf19bae
Norton Nice <nortonnice@gmail.com>
2018-10-22 17:02:24 -0700

    working apps, missing markdown

--- keys.js
+++ keys.js
@@ -1,6 +1,8 @@
-console.log('this is loaded');
+//console.log('this is loaded');
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+/*SPOTIFY STUFF: Client ID 68b77cee22a744f2aa6ab84777018305
+Client Secret a2e52f04048a4f0ca0ce6cb8ac3651a2  */
--- liri.js
+++ liri.js
@@ -1,88 +1,77 @@
+var fs = require("fs");
 var dotEnv = require("dotenv").config();
-var keys = require("keys.js");
-var spotify = new spotify(keys.spotify);
+var keys = require("./keys");
+var moment = require("moment");
+var request = require("request");
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+var searchArg = process.argv.slice(3).join(" ");
+var movieUrl = "http://www.omdbapi.com/?t=" + searchArg + "&y=&plot=short&apikey=trilogy";
+var bandUrl = "https://rest.bandsintown.com/artists/" + searchArg + "/events?app_id=codingbootcamp";
 
-concert-this
-/*`node liri.js concert-this <artist/band name here>`
 
-   * This will search the Bands in Town Artist Events API 
-   * (`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`)
-   *  for an artist and render the following information about each event to the terminal:
 
-     * Name of the venue
 
-     * Venue location
+/*----------MOVIE-THIS----------*/
+if (process.argv[2] === "movie-this")
+  request(movieUrl, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      console.log("TITLE: " + JSON.parse(body).Title + "," + " " + "RELEASED: " +
+        JSON.parse(body).Year + "," + " " + "IMDB RATING: " + JSON.parse(body).imdbRating + "," + " " +
+        "ROTTEN TOMATOES RATING: " + JSON.parse(body).Ratings[1].Value + "," + " " +
+        "PRODUCTION LOCATION: " + JSON.parse(body).Country + "," + " " + "ORIGINAL LANGUAGE: " +
+        JSON.parse(body).Language + "," + " " + "PLOT: " + JSON.parse(body).Plot + "," + " " +
+        "STARING: " + JSON.parse(body).Actors
+      );
+    }
+  });
 
-     * Date of the Event (use moment to format this as "MM/DD/YYYY")*/
-spotify-this-song 
-/*`node liri.js spotify-this-song '<song name here>'`
+/*----------CONCERT-THIS----------*/
+if (process.argv[2] === "concert-this")
+  request(bandUrl, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      console.log("The next show will be in: " + JSON.parse(body)[0].venue.country + ", " +
+        "in the city of " + JSON.parse(body)[0].venue.city + ". " + " The name of the venue is: "
+        + JSON.parse(body)[0].venue.name + ". " + "Date of the show: " + moment(JSON.parse(body)[0].datetime).format("MM/DD/YYYY"));
+    }
+  })
 
-   * This will show the following information about the song in your terminal/bash window
+  /*----------SPOTIFY-THIS----------*/
+if (process.argv[2] === "spotify-this")
+spotify.request("https://api.spotify.com/v1/search?q=" + searchArg + "&type=track&offset=3&limit=3")
+.then(function (data) {
+  console.log("******************************")
+  console.log("Search Result #1:")
+  console.log("Song Name: " + searchArg)
+  console.log("Artist: " + data.tracks.items[0].artists[0].name)
+  console.log("Album Name: " + data.tracks.items[0].album.name)
+  console.log("Preview Song at: " + data.tracks.items[0].preview_url)
+  console.log("******************************")
+  console.log("Search Result #2:")
+  console.log("Song Name: " + searchArg)
+  console.log("Artist: " + data.tracks.items[1].artists[0].name)
+  console.log("Album Name: " + data.tracks.items[1].album.name)
+  console.log("Preview Song at: " + data.tracks.items[1].preview_url)
+  console.log("******************************")
+  console.log("Search Result #3:")
+  console.log("Song Name: " + searchArg)
+  console.log("Artist: " + data.tracks.items[2].artists[0].name)
+  console.log("Album Name: " + data.tracks.items[2].album.name)
+  console.log("Preview Song at: " + data.tracks.items[1].preview_url)
+})
+.catch(function (err) {
+  console.error('Error occurred: ' + err);
+});
 
-     * Artist(s)
+  //do-what-it-says 
+   /*`node liri.js do-what-it-says`*/
+   //if (process.argv[2] === "do-what-it-says")
+    //fs.readFile("random.txt" function())
+  
 
-     * The song's name
 
-     * A preview link of the song from Spotify
 
-     * The album that the song is from
+    //spotify-this-song 
 
-   * If no song is provided then your program will default to "The Sign" by Ace of Base.
 
-   * You will utilize the [node-spotify-api](https://www.npmjs.com/package/node-spotify-api)
-   * package in order to retrieve song information from the Spotify API.
 
-   * The Spotify API requires you sign up as a developer to generate the necessary credentials.
-   * You can follow these steps in order to generate a **client id** and **client secret**:
-
-   * Step One: Visit <https://developer.spotify.com/my-applications/#!/>
-
-   * Step Two: Either login to your existing Spotify account or create a new one 
-   * (a free account is fine) and log in.
-
-   * Step Three: Once logged in, navigate to 
-   * <https://developer.spotify.com/my-applications/#!/applications/create> 
-   * to register a new application to be used with the Spotify API. You can fill in 
-   * whatever you'd like for these fields. When finished, click the "complete" button.
-
-   * Step Four: On the next screen, scroll down to where you see your client id and 
-   * client secret. Copy these values down somewhere, you'll need them to use the Spotify API 
-   * and the [node-spotify-api package](https://www.npmjs.com/package/node-spotify-api).
-*/
-movie-this 
-/*`node liri.js movie-this '<movie name here>'`
-
-   * This will output the following information to your terminal/bash window:
-
-     ```
-       * Title of the movie.
-       * Year the movie came out.
-       * IMDB Rating of the movie.
-       * Rotten Tomatoes Rating of the movie.
-       * Country where the movie was produced.
-       * Language of the movie.
-       * Plot of the movie.
-       * Actors in the movie.
-     ```
-
-   * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-
-     * If you haven't watched "Mr. Nobody," then you should: <http://www.imdb.com/title/tt0485947/>
-
-     * It's on Netflix!
-
-   * You'll use the request package to retrieve data from the OMDB API.
-   *  Like all of the in-class activities, the OMDB API requires an API key. 
-   * You may use `trilogy`.
-*/
-   do-what-it-says 
-   /*`node liri.js do-what-it-says`
-
-   * Using the `fs` Node package, LIRI will take the text inside of random.txt 
-   * and then use it to call one of LIRI's commands.
-
-     * It should run `spotify-this-song` for "I Want it That Way," as follows
-     *  the text in `random.txt`.
-
-     * Edit the text in random.txt to test out the feature for movie-this and concert-this.*/
-    
